531293fd08c6f42a310260d26f401787c72f228e
Dax Ravi <d@ironbay.digital>
2017-07-07 11:17:06 -0400

    Sync

---
+++ shell/dotfiles/.i3/workspace.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+i3-msg 'workspace 1; append_layout ~/.i3/workspace-1.json'
+i3-msg "workspace 1; exec google-chrome-stable"
+i3-msg "workspace 1; exec slack"
+i3-msg "workspace 1; exec google-chrome-stable --profile-directory=Default --app-id=fpniocchabmgenibceglhnfeimmdhdfm"
+i3-msg "workspace 2; exec urxvt"
+
+PROJECT=~/dev/projects/ridehealth/ridehealth-web
+i3-msg 'workspace 2; append_layout ~/.i3/workspace-2.json'
+i3-msg "workspace 2; exec urxvt -name ridehealth-web -e /usr/sbin/zsh -is eval 'cd $PROJECT'"
+i3-msg "workspace 2; exec urxvt -name ridehealth-web-console -e /usr/sbin/zsh -is eval 'cd $PROJECT';"
+i3-msg "workspace 2; exec code $PROJECT;"
+
+PROJECT=~/dev/projects/ridehealth/ridehealth
+i3-msg 'workspace 3; append_layout ~/.i3/workspace-3.json'
+i3-msg "workspace 3; exec urxvt -name ridehealth -e /usr/sbin/zsh -is eval 'cd $PROJECT'"
+i3-msg "workspace 3; exec urxvt -name ridehealth-console -e /usr/sbin/zsh -is eval 'cd $PROJECT';"
+i3-msg "workspace 3; exec code $PROJECT;"
