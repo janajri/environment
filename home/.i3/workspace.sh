af32222659a5a659b48b9a1553667cf9d0a337b4
Dax Ravi <d@ironbay.digital>
2017-07-14 11:29:46 -0400

    Unescape

--- home/.i3/workspace.sh
+++ home/.i3/workspace.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-# i3-msg 'workspace 1; append_layout ~/.i3/main.json'
-# i3-msg "workspace 1; exec google-chrome-stable"
-# i3-msg "workspace 1; exec slack"
-# i3-msg "workspace 1; exec google-chrome-stable --profile-directory=Default --app-id=fpniocchabmgenibceglhnfeimmdhdfm"
-# i3-msg "workspace 1; exec urxvt"
+i3-msg 'workspace 1; append_layout ~/.i3/main.json'
+i3-msg "workspace 1; exec google-chrome-stable"
+i3-msg "workspace 1; exec slack"
+i3-msg "workspace 1; exec google-chrome-stable --profile-directory=Default --app-id=fpniocchabmgenibceglhnfeimmdhdfm"
+i3-msg "workspace 1; exec urxvt"
 
 PROJECT=~/dev/projects/ridehealth/ridehealth-web
 i3-msg 'workspace 2; append_layout ~/.i3/code.json'
